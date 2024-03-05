ee4fde0013da068d37331e795d57de3316528386
v7nayak <103919203+v7nayak@users.noreply.github.com>
2024-03-05 20:09:35 +0530

    Update practice13.java

--- practice13.java
+++ practice13.java
@@ -2,7 +2,7 @@
 
 import java.util.ArrayList;
 
-public class practice16 {
+public class practice13 {
     public static void main(String[] args) {
         String up = "abc";
         System.out.println(subset(up, ""));
