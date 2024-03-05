eae5f51bf46d64f7734cea8fc3cb74c0cc3cc13e
v7nayak <103919203+v7nayak@users.noreply.github.com>
2024-03-05 20:08:12 +0530

    Add files via upload

---
+++ practice13.java
@@ -0,0 +1,30 @@
+//Day 13 - Subsets
+
+import java.util.ArrayList;
+
+public class practice16 {
+    public static void main(String[] args) {
+        String up = "abc";
+        System.out.println(subset(up, ""));
+    }
+    static ArrayList<String> subset(String up, String p){
+
+
+
+        if(up.isEmpty()){
+            ArrayList<String> list1 = new ArrayList<>();
+            list1.add(p);
+            return list1;
+        }
+
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> ans = subset(up.substring(1), p);
+        ArrayList<String> ans2 = subset(up.substring(1), p+ch);
+
+        ans2.addAll(ans);
+        return ans2;
+    }
+
+}
