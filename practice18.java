eeb028d289f7ec9b41f0b313e1733e60f92a594e
v7nayak <103919203+v7nayak@users.noreply.github.com>
2024-03-11 21:58:14 +0530

    Add files via upload

---
+++ practice18.java
@@ -0,0 +1,35 @@
+//Day 18 - Permutation Questions
+// ***
+//Leetcode problem 17 - SKIPPED
+
+import java.util.ArrayList;
+
+public class practice18 {
+    public static void main(String[] args) {
+        String nums = "12";
+//        getAns(nums);
+    }
+
+
+//    static ArrayList<String> getAns(String nums){
+//
+//
+//            char one = nums.charAt(0);
+//            char two = nums.charAt(1);
+//            char three = nums.charAt(2);
+//            char four = nums.charAt(3);
+//
+//
+//        char num1 = nums.charAt(0);
+//
+//        String[] arr2 = {"a","b","c"};
+//        String[] arr3 = {"d","e","f"};
+//        String[] arr4 = {"g","h", "i"};
+//        String[] arr5 = {"j", "k", "l"};
+//        String[] arr6 = {"m", "n", "o"};
+//        String[] arr7 = {"p", "q", "r", "s"};
+//        String[] arr8 = {"t", "u", "v"};
+//        String[] arr9 = {"w", "x", "y", "z"};
+//    }
+//    return ArrayList<String> list = new ArrayList<>();
+}
